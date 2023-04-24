7541a6e3e4e8a7d08075160934513a26068a0cb1
adlarajasri <129931618+adlarajasri@users.noreply.github.com>
2023-04-24 09:20:41 +0530

    Add files via upload

---
+++ InheritanceTel/InheritanceTel/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+namespace InheritanceTel
+{
+    class Telephone
+    {
+        protected string _phonetype;
+        public string phonetype
+        {
+            get 
+            { 
+                return _phonetype; 
+            } 
+        set
+            {
+                _phonetype = value;
+            }
+        }
+        public Telephone() 
+        {
+        
+        }
+        public Telephone(string phType)
+        {
+            _phonetype = phType;
+        }
+        public void Ring()
+        {
+            Console.WriteLine($"Ringing the {_phonetype}");
+        }
+
+        class Electronicphone:Telephone
+        {
+            public Electronicphone() 
+            {
+                _phonetype = "Digital";
+            }
+            public void Run()
+            {
+                base.Ring();
+            }
+        }
+        class program
+        {
+            static void Main(string[] args) 
+            {
+                Electronicphone phone = new Electronicphone();
+                phone.Run();
+            }
+        }
+    }
+}
