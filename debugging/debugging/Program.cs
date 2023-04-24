8cf451de792648b748a78f63f6df75d823729c33
adlarajasri <129931618+adlarajasri@users.noreply.github.com>
2023-04-24 09:19:10 +0530

    Add files via upload

---
+++ debugging/debugging/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace debugging
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter First No:");
+            int i = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter Second No:");
+            int j = int.Parse(Console.ReadLine());
+            foo(i, j);
+            Console.WriteLine("debugging Over");
+        }
+
+        public static void foo(int x, int y)
+        {
+            int t = 100;
+            for(int i=x; i<y; i++) 
+            {
+                Console.WriteLine("India");
+                t = t + 34;
+            }
+        }
+    }
+}
